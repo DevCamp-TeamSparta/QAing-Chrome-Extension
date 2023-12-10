ca12cda70359ff9d04eb94eca8d941ff4f2ac4f6
rhsok <rhsok823@gmail.com>
2023-12-10 18:45:45 +0900

    Feat: 쓰로틀링 적용

--- src/contentScript/components/Recorder.tsx
+++ src/contentScript/components/Recorder.tsx
@@ -1,7 +1,7 @@
-import React from 'react'
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import axios from 'axios'
 import { useInterval } from '../hooks/useInterval'
+import React from 'react'
 
 function Recorder() {
 	const [recording, setRecording] = useState(false)
@@ -12,67 +12,63 @@ function Recorder() {
 	//토큰
 	const [accessToken, setAccessToken] = useState(null)
 
-	//계속 진행되는 타이머
+	//타이머
 	const [time, setTime] = useState<number>(0)
 	const [isRunning, setIsRunning] = useState<boolean>(false)
 	const [timeRecords, setTimeRecords] = useState<number[]>([])
-	//15분 마다 리셋되는 타이머
-	const [TimerBeingReset, setTimerBeingReset] = useState<number>(0)
-	const [isRunningTimerBeingReset, setIsRunningTimerBeingReset] =
-		useState<boolean>(false)
+
+	const recordTimeout = useRef<NodeJS.Timeout | null>(null)
 
 	//화면녹화 버튼을 감지해서 녹화를 실행하는 코드
 	useEffect(() => {
 		if (recording) {
-			// 미디어 스트림 획득
-			navigator.mediaDevices
-				.getDisplayMedia({ video: true })
-				.then((stream: MediaStream) => {
-					handleStartStop()
-					resetTimerhandleStartStop()
-					// setMediaStream(stream)
-					const recorder = new MediaRecorder(stream)
-					setMediaRecorder(recorder)
-
-					recorder.ondataavailable = (e) => {
-						if (e.data.size > 0) {
-							const chunks: Blob[] = [e.data]
-							const blob = new Blob(chunks, { type: 'video/webm' }) // Blob 객체 생성
-							setRecordedChunks(blob)
-							// onSubmitVideo(blob, timeArray)
-							console.log('chunks', chunks)
-						}
-					}
-
-					recorder.start(20000)
-
-					// 5분 후에 녹화 중지
-					const timeoutId = setTimeout(
-						() => {
-							recorder.stop()
-							if (mediaStream) {
-								mediaStream.getTracks().forEach((track) => track.stop())
-							}
-						},
-						60 * 1000 + 2,
-					) // 5분
-
-					return () => clearTimeout(timeoutId) // 컴포넌트 unmount 시 타이머 해제
-				})
-				.catch((error) => {
-					console.error('Error accessing media devices:', error)
-				})
+			startRecording()
+			const timer = setTimeout(() => {
+				stopRecording()
+			}, 20000) // 20초 후에 녹화 중단
+			return () => clearTimeout(timer)
 		} else {
-			// 녹화 중지
-			if (mediaRecorder) {
-				mediaRecorder.stop()
+			stopRecording()
+		}
+	}, [recording])
+
+	const startRecording = () => {
+		navigator.mediaDevices.getDisplayMedia({ video: true }).then((stream) => {
+			const recorder = new MediaRecorder(stream)
+			setMediaRecorder(recorder)
+			const chunks: Blob[] = []
+			recorder.ondataavailable = (e) => {
+				if (e.data.size > 0) {
+					chunks.push(e.data)
+					setRecordedChunks(new Blob([e.data], { type: 'video/webm' }))
+					console.log('chunks', chunks)
+				}
 			}
+
+			recorder.onstop = () => {
+				const blob = new Blob(chunks, { type: 'video/webm' })
+				const url = URL.createObjectURL(blob)
+				setVideoURL(url)
+				console.log('url', url)
+			}
+
+			recorder.start()
+			setIsRunning(true)
+			setMediaStream(stream)
+		})
+	}
+
+	const stopRecording = () => {
+		if (mediaRecorder) {
+			mediaRecorder.stop()
 			if (mediaStream) {
 				mediaStream.getTracks().forEach((track) => track.stop())
 			}
+			setRecording(false)
+			setIsRunning(false)
+			setTime(0)
 		}
-		// console.log('recordedChunks', recordedChunks)
-	}, [recording])
+	}
 
 	useEffect(() => {
 		if (recordedChunks) {
@@ -83,12 +79,14 @@ function Recorder() {
 	//녹화 시작 정지 버튼핸들러
 	const handleStartStopClick = () => {
 		setRecording((prevRecording) => !prevRecording)
-
 		if (recording) {
-			setIsRunning((prevIsRunning) => !prevIsRunning)
-			setIsRunningTimerBeingReset((prevIsRunning) => !prevIsRunning)
+			handleStartStop()
 			setTime(0)
-			setTimerBeingReset(0)
+		}
+		if (!recording) {
+			setTime(0)
+			setTimeRecords([])
+			handleStartStop()
 		}
 	}
 
@@ -114,23 +112,21 @@ function Recorder() {
 		}
 	}
 
-	// const handleDownloadClick = () => {
-	// 	// 녹화된 비디오 다운로드
-
-	// 	if (recordedChunks.length > 0) {
-	// 		const blob = new Blob(recordedChunks, { type: 'video/webm' })
-	// 		console.log('전송시작')
-	// 		const url = URL.createObjectURL(blob)
-	// 		const a = document.createElement('a')
-	// 		a.href = url
-	// 		a.download = 'recorded-screen.webm'
-	// 		document.body.appendChild(a)
-	// 		a.click()
-	// 		URL.revokeObjectURL(url)
-	// 		document.body.removeChild(a)
-	// 		console.log('a')
-	// 	}
-	// }
+	const handleDownloadClick = (blob: Blob) => {
+		// 녹화된 비디오 다운로드
+		if (recordedChunks) {
+			console.log('비디오 다운로드 준비완료')
+			const url = URL.createObjectURL(blob)
+			const a = document.createElement('a')
+			a.href = url
+			a.download = 'recorded-screen.webm'
+			document.body.appendChild(a)
+			a.click()
+			URL.revokeObjectURL(url)
+			document.body.removeChild(a)
+			console.log('다운 가자!')
+		}
+	}
 
 	useEffect(() => {
 		console.log('timeRecords', timeRecords)
@@ -144,24 +140,18 @@ function Recorder() {
 		isRunning ? 1000 : null,
 	)
 
-	//15분마다 초기화 되게 할 타이머
-	useInterval(
-		() => {
-			setTimerBeingReset((prevTime) => prevTime + 1)
-		},
-		isRunningTimerBeingReset ? 1000 : null,
-	)
-
 	const handleStartStop = () => {
 		setIsRunning((prevIsRunning) => !prevIsRunning)
 	}
 
-	const resetTimerhandleStartStop = () => {
-		setIsRunningTimerBeingReset((prevIsRunning) => !prevIsRunning)
-	}
-
 	const handleRecordTime = () => {
-		setTimeRecords((prevRecords) => [...prevRecords, time])
+		setTimeRecords((prevRecords) => {
+			const lastRecord = prevRecords[prevRecords.length - 1]
+			if (lastRecord !== time) {
+				return [...prevRecords, time]
+			}
+			return prevRecords
+		})
 	}
 
 	const handleReset = () => {
@@ -189,6 +179,29 @@ function Recorder() {
 	//크롬 익스텐션이 실행 됬을 때 토큰 background.js를 이용해 chrome.cookie에서 토큰을 가져온다.
 	useEffect(() => {
 		// alert('익스텐션 시작')
+		// const currentUrl = window.location.origin
+		// console.log('currentUrl', currentUrl)
+		// console.log('currentUrl', currentUrl === 'https://app.qaing.co')
+		// chrome.runtime?.sendMessage({ action: 'getToken' }, (response) => {
+		// 	if (response.accessToken) {
+		// 		setAccessToken(response.accessToken)
+		// 	}
+		// 	else {
+		// 		// alert('로그인이 필요합니다.')
+		// 		if (
+		// 			currentUrl === 'https://app.qaing.co' ||
+		// 			currentUrl === 'http://localhost:3000' ||
+		// 			currentUrl === 'https://accounts.google.com' ||
+		// 			currentUrl === 'https://test.qaing.co'
+		// 		) {
+		// 			return
+		// 		}
+		// 		window.location.href = 'https://app.qaing.co/auth/signup'
+		// 	}
+		// })
+	}, [])
+
+	const isLogin = () => {
 		const currentUrl = window.location.origin
 		console.log('currentUrl', currentUrl)
 		console.log('currentUrl', currentUrl === 'https://app.qaing.co')
@@ -196,9 +209,9 @@ function Recorder() {
 		chrome.runtime?.sendMessage({ action: 'getToken' }, (response) => {
 			if (response.accessToken) {
 				setAccessToken(response.accessToken)
-				alert('로그인이 되었습니다.')
+				handleStartStopClick()
 			} else {
-				// alert('로그인이 필요합니다.')
+				alert('로그인이 필요합니다.')
 				if (
 					currentUrl === 'https://app.qaing.co' ||
 					currentUrl === 'http://localhost:3000' ||
@@ -210,7 +223,7 @@ function Recorder() {
 				window.location.href = 'https://app.qaing.co/auth/signup'
 			}
 		})
-	}, [])
+	}
 
 	useEffect(() => {
 		console.log('accessToken', accessToken)
@@ -225,7 +238,7 @@ function Recorder() {
 						<div className="flex flex-row items-center justify-center ">
 							<button
 								className="bg-[#E95050] w-[24px] h-[24px] m-auto rounded-sm"
-								onClick={handleStartStopClick}
+								onClick={isLogin}
 							>
 								{recording ? '정지' : ''}
 							</button>
@@ -255,11 +268,6 @@ function Recorder() {
 								<div className="font-semibold text-xl">
 									<button onClick={handleRecordTime}>이슈저장</button>
 								</div>
-								{/* <button onClick={handleStartStop}>
-									{isRunning ? 'Stop' : 'Start'}
-								</button>
-								<button onClick={handleRecordTime}>타임 기록</button>
-								<button onClick={handleReset}>리셋</button> */}
 								<p>{`${Math.floor(time / 60)
 									.toString()
 									.padStart(2, '0')}:${(time % 60)
@@ -277,7 +285,7 @@ function Recorder() {
 						<button
 							type="submit"
 							className="w-[400px] h-[100px] bg-white"
-							// onClick={handleDownloadClick}
+							onClick={() => handleDownloadClick}
 						>
 							전송하기
 						</button>
