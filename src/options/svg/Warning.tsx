1a5b3baa24e0831702351553f4658280f70bdd28
rhsok <117060851+rhsok@users.noreply.github.com>
2024-01-04 13:49:31 +0900

    Feat: 옵션페이지 이미지수정

---
+++ src/options/svg/Warning.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export function Warning() {
+	return (
+		<svg
+			width="36"
+			height="36"
+			viewBox="0 0 36 36"
+			fill="none"
+			xmlns="http://www.w3.org/2000/svg"
+		>
+			<path
+				d="M18 27C22.9706 27 27 22.9706 27 18C27 13.0294 22.9706 9 18 9C13.0294 9 9 13.0294 9 18C9 22.9706 13.0294 27 18 27Z"
+				fill="#FF5847"
+			/>
+			<path
+				fill-rule="evenodd"
+				clip-rule="evenodd"
+				d="M33 18C33 26.2843 26.2843 33 18 33C9.71573 33 3 26.2843 3 18C3 9.71573 9.71573 3 18 3C26.2843 3 33 9.71573 33 18ZM30 18C30 24.6274 24.6274 30 18 30C11.3726 30 6 24.6274 6 18C6 11.3726 11.3726 6 18 6C24.6274 6 30 11.3726 30 18Z"
+				fill="#FF5847"
+			/>
+		</svg>
+	)
+}
